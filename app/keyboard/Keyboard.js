39a2afacea9a579780aef7a60b31de6024176252
Szilágyi Balázs <zyxneo@gmail.com>
2017-04-12 19:00:58 +0200

    Enter formatted

--- app/keyboard/Keyboard.js
+++ app/keyboard/Keyboard.js
@@ -39,3 +39,10 @@ export default class Keyboard extends React.Component {
     )
   }
 }
+
+
+/*
+The harmonized 48 graphic key keyboard arrangement
+Keyboards which comply to this narrower specification contain all the keys shown in white in the figure above, the key at C12 shown in yellow, and one of the two keys at E13 and B00 shown in red. The standard does not require this; it only says that keyboards complying to this narrower specification can be called such.
+In fact, several layouts (e. g. the US layout), to allow a wider return key, have a key at D13 (shown in green) instead of C12 (shown in yellow). Thus, while they cannot be called “harmonized 48 graphic key keyboards” according to the standard, they still comply to the standard itself. It is to be noted that ISO/IEC 9995-3:2010, in referring to the basic layout within its specific scope, does take a possible substitution of C12 by D13 into account.
+*/
--- app/keyboard/KeyboardKey.js
+++ app/keyboard/KeyboardKey.js
@@ -1,6 +1,9 @@
 require("./keyboard-key.scss");
 
 import React, {PropTypes} from 'react';
+
+import KeyboardKeyEnter from "./KeyboardKeyEnter";
+
 import KeyboardKeyShift from "./KeyboardKeyShift";
 import KeyboardKeyCaps from "./KeyboardKeyCaps";
 import KeyboardKeyCs from "./KeyboardKeyCs";
@@ -11,21 +14,30 @@ import KeyboardKeyTo from "./KeyboardKeyTo";
 
 export default class KeyboardKey extends React.Component {
   render () {
-    let rowClass = "key--" + this.props.iso.substring(0, 1);
+    let keyClass = "key key--" + this.props.iso.substring(0, 1) + " " + this.props.iso + " " + this.props.state;
 
-    return (
-      <g className={"key " + rowClass + " " + this.props.iso + " " + this.props.state}>
-        <rect className="key__bg"/>
-        <g className="key__labels" textAnchor="middle">
-          <KeyboardKeyShift shift={this.props.shift}/>
-          <KeyboardKeyCaps caps={this.props.caps}/>
-          <KeyboardKeyCs cs={this.props.cs}/>
-          <KeyboardKeyAltGr altgr={this.props.altgr}/>
-          <KeyboardKeyCc cc={this.props.cc}/>
-          <KeyboardKeyTo to={this.props.to}/>
+    // When letters on a case pair are associated with a key, only the capital character need to be shown on the keytop for the primary group, while the lowercase character only is shown for the secondary group.
+    if (this.props.to.toUpperCase() === this.props.shift) {
+      keyClass += " alpha";
+    }
+
+    if (this.props.iso === "D13") {
+      return <KeyboardKeyEnter/>
+    } else {
+      return (
+        <g className={keyClass}>
+          <rect className="key__bg"/>
+          <g className="key__labels" textAnchor="middle">
+            <KeyboardKeyTo to={this.props.to}/>
+            <KeyboardKeyShift shift={this.props.shift}/>
+            <KeyboardKeyCaps caps={this.props.caps}/>
+            <KeyboardKeyCs cs={this.props.cs}/>
+            <KeyboardKeyAltGr altgr={this.props.altgr}/>
+            <KeyboardKeyCc cc={this.props.cc}/>
+          </g>
         </g>
-      </g>
-    );
+      );
+    }
   }
 }
 

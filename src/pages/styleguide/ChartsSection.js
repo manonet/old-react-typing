a546385ed60f682225e32e58225788d58ee220dc
Szilágyi Balázs <zyxneo@gmail.com>
2017-06-11 17:58:35 +0200

    pieChart variants

--- src/pages/styleguide/ChartsSection.js
+++ src/pages/styleguide/ChartsSection.js
@@ -11,21 +11,57 @@ function toRadians (angle) {
   return angle * (Math.PI / 180);
 }
 
+function PieChartDeco1 (props) {
+  if (props.decorated) {
+    return (
+      <circle cx={props.cx}
+              cy={props.cy}
+              r={props.r}
+              stroke={props.stroke}
+              fill={props.fill}
+              strokeWidth={props.strokeWidth}
+              strokeDasharray={props.strokeDasharray}
+              strokeDashoffset={props.strokeDashoffset || 0}
+              transform={props.transform}
+      />
+    );
+  } else {
+    return null;
+  }
+
+}
+
 function PieChart (props) {
   // props
+  let unitAll = props.unitAll;
+  let unitPart = props.unitPart;
+  let percent = props.percent || unitPart / unitAll * 100 || 0;
+
   let archWidth = props.archWidth || 10;
   let r = props.r || 50 // radius
   let rArch = r - archWidth / 2;
   let padding = props.padding || 20;
-  let percent = props.percent || 0;
   let color = props.color || "hotpink";
   let bgcolor = props.bgcolor || "#ddd";
   let strokeLinecap = props.strokeLinecap || "butt";
   let rotation = props.rotation || 0; // 0 - default left
   let circleFill = props.circleFill || "none";
+  let filter = props.filter || "none";
+
   // animation
   let repeatCount = props.repeatCount || 1; // 0, 1, ... indefinite
   let dur = props.dur || "2s"; // duration
+  // decoration
+  let decorated = props.decorated || false;
+  let decoStrokeWidth = props.decoStrokeWidth || 5;
+  let decoR = props.decoR - decoStrokeWidth / 2 || rArch + archWidth / 2 + 5;
+  let decoStroke = props.decoStroke || "#000";
+  let decoFill = props.decoFill || "none";
+  let decoStrokeDashWidth = props.decoStrokeDashWidth || 2;
+  let decoC = 2 * decoR * Math.PI; // circumference of deco circle
+  let decoStrokeDasharray = props.decoStrokeDasharray || decoStrokeDashWidth + "," + (decoC / (unitAll || 100) - decoStrokeDashWidth)  || "1,3";
+  let c = 2 * rArch * Math.PI; // circumference of deco circle
+  let strokeDasharray = props.strokeDasharray || (c / (unitAll / 10) - decoStrokeDashWidth) + "," + decoStrokeDashWidth || "";
 
   // calculated values
   let cx = r + padding;
@@ -52,13 +88,11 @@ function PieChart (props) {
 
   return (
     <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
-      <filter id="dropshadow"  x="-10" y="-10" width={width + 20} height={height + 20}>
-        <feGaussianBlur in="SourceAlpha" stdDeviation="3"/>
-        <feOffset dx="1" dy="1" result="offsetblur"/>
-        <feMerge>
-          <feMergeNode/>
-          <feMergeNode in="SourceGraphic"/>
-        </feMerge>
+      <filter id="insetshadow"  x="-10" y="-10" width={width + 20} height={height + 20}>
+        <feFlood floodColor="black"/>
+        <feComposite operator="out" in2="SourceGraphic"/>
+        <feGaussianBlur stdDeviation="2"/>
+        <feComposite operator="atop" in2="SourceGraphic"/>
       </filter>
 
       <circle cx={cx}
@@ -66,7 +100,22 @@ function PieChart (props) {
               r={rArch}
               stroke={bgcolor}
               fill={circleFill}
-              strokeWidth={archWidth}/>
+              strokeWidth={archWidth}
+              strokeDasharray={strokeDasharray}
+              strokeDashoffset={-decoStrokeDashWidth}
+              transform={"rotate(" + (rotation + 180) + " " + cx + " " + cy + ")"}/>
+
+      <PieChartDeco1
+        decorated={decorated}
+        cx={cx}
+        cy={cy}
+        r={decoR}
+        stroke={decoStroke}
+        fill={decoFill}
+        strokeWidth={decoStrokeWidth}
+        strokeDasharray={decoStrokeDasharray}
+        transform={"rotate(" + (rotation + 180) + " " + cx + " " + cy + ")"}
+      />
 
       <path className="path" d={"M" + arcStartX + " " + arcStartY +
               "A " + rArch + " " + rArch + " 0 " + largeArcFlag + " 1 " + arcEndX + " " + arcEndY
@@ -76,7 +125,9 @@ function PieChart (props) {
               strokeLinecap={strokeLinecap}
               strokeWidth={archWidth}
               strokeDasharray={s}
-              strokeDashoffset={s}>
+              strokeDashoffset={s}
+              filter={filter}
+      >
 
         <animate attributeType="XML" attributeName="stroke-dashoffset" from={s} to="0" dur={dur} repeatCount={repeatCount} fill="freeze"/>
       </path>
@@ -84,6 +135,40 @@ function PieChart (props) {
   );
 }
 
+function PieChart1 (props) {
+  return (
+    <PieChart
+      r={100}
+      percent={props.percent}
+      archWidth={100}
+      color="fuchsia"
+      bgcolor="yellow"
+      decorated={true}
+    />
+  )
+}
+
+function PieChart2 (props) {
+  return (
+    <PieChart
+      r={80}
+      unitAll={props.unitAll}
+      unitPart={props.unitPart}
+      percent={props.percent}
+      rotation={90}
+      archWidth={14}
+      color="skyblue"
+      bgcolor="#777"
+      circleFill="#eee"
+
+      decorated={true}
+      decoR={"80"}
+      decoStroke="#eee"
+      decoFill={"none"}
+      decoStrokeWidth={7}
+    />
+  )
+}
 
 export default class ChartsSection extends React.Component {
   render() {
@@ -103,22 +188,12 @@ export default class ChartsSection extends React.Component {
             strokeLinecap="round"
             color="darkviolet"
           />
-          <PieChart
-            r={100}
+          <PieChart1
             percent={65.5}
-            archWidth={100}
-            color="fuchsia"
-            bgcolor="yellow"
           />
-          <PieChart
-            r={80}
-            percent={35.5}
-            rotation={90}
-            archWidth={15}
-            strokeLinecap="round"
-            color="skyblue"
-            bgcolor="#777"
-            circleFill="#eee"
+          <PieChart2
+            unitAll={150}
+            unitPart={41}
           />
           <PieChart
             r={60}
@@ -128,7 +203,9 @@ export default class ChartsSection extends React.Component {
             strokeLinecap="round"
             color="#79bd9a"
             bgcolor="none"
+            filter={`url(#insetshadow)`}
           />
+
 
         </section>
       )
